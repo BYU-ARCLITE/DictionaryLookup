02569e101f99794ebe287472421018b033e8a28d
dallasvw <i.am.dallas@hotmail.com>
2014-03-15 00:20:41 -0600

    Fixed Special Cases for Merriam-Webster
    Added option to reduce definition count
    Removed duplicated audio files

--- app/controllers/services/MerriamWebster.scala
+++ app/controllers/services/MerriamWebster.scala
@@ -16,40 +16,65 @@ object LookupMerriamWebster extends Translator {
   val expiration = Utils.getExpiration("merriamWebster")
   val merriamWebsterSpanishKey = configuration.getString("merriamWebster.spanishKey")
   val merriamWebsterCollegiateKey = configuration.getString("merriamWebster.collegiateKey")
-    
+
   def parseXMLResponse(URL:String, headWordTag:String) : Seq[String] = {
     val (true, body) = try {
       val url = new URL(URL)
       val body = url.openStream
       (true, body)
     } catch { case ex:Exception => (false, null) }
-    
+
     val XMLDoc = XML.load(body)
     val response = (XMLDoc \\ "entry_list") 
-    def defList : Seq[String] = for {
+    val defList : Seq[String] = for {
       entry <- response \\ "entry"
       defins <- entry \\ "dt"
       } yield {   
         val word = (entry \\ headWordTag).text
         val wav = (entry \\ "wav").text
         val defns = defins.text
-        "(" + word + ")" + defns //+ " Sound: " + wav
+        "(" + word + ")" + defns
     }
-    
+	
+	def isNum(item:String) : Boolean = {
+		val bool = if ((item == '0')||(item == '1')||(item == '2')||(item == '3')||(item == '4')||(item == '5')||(item == '6')||(item == '7')||(item == '8')||(item == '9'))
+		  true
+		false
+	}
+	
     val sound : Seq[String] = for {
       entry <- XMLDoc \\ "wav"
       } yield {   
         val file = entry.text
-        val firstLetter = file.split(" ").flatMap(_.headOption).mkString
-        val html = "<audio controls class='merriamAudio'><source src='http://media.merriam-webster.com/soundc11/" + firstLetter + "/" + file + "' type='audio/wav'>Audio File Not Found.</audio>"
+		val dir = if(file.substring(0,3) == "bix" )
+		      "bix"
+			else if (file.substring(0,2) == "gg")
+			  "gg"
+			else if (isNum(file.substring(0,1)))
+			  "number"
+			else
+			  file.substring(0,1)
+        val html = "<audio controls class='merriamAudio'><source src='http://media.merriam-webster.com/soundc11/" + dir + "/" + file + "' type='audio/wav'>Audio File Not Found.</audio>"
         html
     }
-    
-    val defs = defList:+ "Audio Examples:"
-    defs ++ sound
-    
+
+	/* If you want to reduce the amount of definitions or audio files, uncomment below*/ 
+	/*val exampleSound : Seq[String] = if(sound.length > 0)
+		{val snd : Seq[String] = Seq[String]("<br/>Audio Examples:", sound(0)); snd} else {sound}
+    val definitions : Seq[String] = if(defList.length > 5)
+	    {
+		    val defin : Seq[String] = Seq[String](defList(0), defList(1), defList(2), defList(3), defList(4))
+			defin
+		} else {defList}
+
+    definitions ++ exampleSound*/
+	
+	val exampleSound : Seq[String] = if(sound.length > 0)
+		{"<br/>Audio Examples:" +: sound} else {sound}
+	defList ++ exampleSound.distinct
+	
   }
-  
+
   /**
    * Endpoint for translating via merriamWebster
    */
