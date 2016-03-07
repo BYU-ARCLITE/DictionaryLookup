a8fa02e324fe5fae0a7d3a3a41ffb6c1861a8a48
Logan Kearsley <chronosurfer@gmail.com>
2016-03-07 16:30:50 -0700

    Start working on segmenting in WWWJDIC

--- app/controllers/services/WWWJDIC.scala
+++ app/controllers/services/WWWJDIC.scala
@@ -7,10 +7,13 @@ import scala.concurrent.{ExecutionContext, Future, Await}
 import scala.concurrent.duration._
 import play.api.mvc._
 import play.api.libs.json._
-import play.api.libs.ws.WS
+import play.api.libs.ws.{WS, WSResponse}
 import play.api.Play.{current, configuration}
 import ExecutionContext.Implicits.global
 import xml.{Elem, XML, NodeSeq}
+
+case class JAnalysis(start: Long, end: Long,
+                     lemma: String, gloss: JsObject)
 
 object LookupWWWJDIC extends Translator {
   /**
@@ -28,6 +31,7 @@ object LookupWWWJDIC extends Translator {
   val name = "WWWJDIC"
   val expiration = Utils.getExpiration("WWWJDIC")
   val codeFormat = 'iso639_1
+  val endpoint =  "http://nihongo.monash.edu/cgi-bin/wwwjdic"
 
   /** Maps the languages to the dictionary letter **/
   val codeMap = Map("en" -> "1",
@@ -46,79 +50,116 @@ object LookupWWWJDIC extends Translator {
     codes.map((_,"ja")) ++ codes.map(("ja",_))	
   }
 
-  /**
-   *  Translate To and From Japanese for all of the languages in the list above.
-   *  Uses the xml.Elem and XML class
-   *  Can't be loaded by the xml parser for 3 reasons:
-   *    1. Needs a systemId. The first .replace() gets rid of the doctype tag to avoid this error
-   *    2. There is no matching </META> tag. The second .replace() gets rid of the <META...>.
-   *    3. There is a random <p> with no matching </p> in every response. The third .replace() gets rid of it.
-   *  Query String:
-   *    1: the en-ja dictionary (replace with Q for comprehensive dictionary that returns results from multiple dictionaries)
-   *    Z: backdoor entry. Returns definitions as text inside <pre>...</pre> element. (replace with M for a fully formatted HTML response)
-   *    U: results in UTF-8
-   *    Q: get exact matches (replace with P = common matches, R = P && Q, E every match)
-   */
-  def getTranslations(text: String, dictCode: String) = {
-    val query = WS.url("http://nihongo.monash.edu/cgi-bin/wwwjdic?" + dictCode + "ZUQ" + text).get()
-
-    // Get Results, remove newlines for ease of use with re
+  def getXML(query: Future[WSResponse]) = {
     val response = Await.result(query, Duration.Inf)
     if (response.status != 200) None
     else {
-      // Clean up the html to be read by xml
-      val data = response.body.replace("""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">""", "").replace(
-        """<META http-equiv="Content-Type" content="text/html; charset=UTF-8">""", "").replace("""<p>""", "")
+      val data = response.body
+                 .replace("""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">""", "")
+                 .replace("""<META http-equiv="Content-Type" content="text/html; charset=UTF-8">""", "")
+                 .replace("""<p>""", "")
+                 .replace("""<br>""", "<br/>")
 
-      val XMLdoc = XML.loadString(data)
-      var raw = (XMLdoc \\ "pre").text.split("\n")
-      val defins = raw.flatMap(_.split("""[,\s\/]*\(\d+\)"""))
-                      .map(_.trim).distinct
-                      .filterNot(_ == "")
-      if (defins.length == 0) None
-      else Some(defins)
+      Some(XML.loadString(data))
     }
   }
 
-  /* TODO: Make use of the word breaking / glossing feature */
-  def translate(user: User, src: String, dst: String, text: String)
-               (implicit request: RequestHeader, restart: TRestart) = {
-    (if (src == "ja") {
-      codeMap.get(dst).flatMap { dcode =>
-        getTranslations(text, dcode)
-      }
-    } else if (dst == "ja") {
-      codeMap.get(src).flatMap { dcode =>
-        getTranslations(text, dcode)
-      }
-    } else None).map { defs =>
-      val senses = defs.map { text =>
-        Json.obj("definition" -> text)
-      }
-      Json.obj(
-        //"translations" -> Json.arr("free translation text")
-        "words" -> Json.arr(
-          Json.obj(
-            "start" -> 0,
-            "end" -> text.length,
-            "lemmas" -> Json.arr(
-              Json.obj(
-                "representations" -> Json.arr("Orthographic"),
-                "lemmaForm" -> "lemma",
-                "forms" -> Json.obj(
-                  "lemma" -> Json.obj(
-                    "Orthographic" -> Json.arr(text)
-                  )
-                ),
-                "senses" -> senses,
-                "sources" -> Json.arr(
-                  Json.obj("name" -> name, "attribution" -> s"<i>$name</i>")
-                )
+  def getTokens(text: String) = {
+    val query = WS.url(s"$endpoint?9ZIG$text").get()
+    getXML(query).map { XMLDoc =>
+      /*val glosses = (XMLDoc \\ "li").map { node =>
+        val entry = node.text
+        val word = ""
+        val pos = ""
+
+        val gloss = Json.obj(
+          "start" -> start,
+          "end" -> end,
+          "lemmas" -> Json.arr(
+            Json.obj(
+              "representations" -> Json.arr("Orthographic"),
+              "pos" -> pos,
+              "lemmaForm" -> "lemma",
+              "forms" -> Json.obj(
+                "lemma" -> Json.obj("Orthographic" -> Seq(word))
+              ),
+              "senses" -> gloss.map { dt =>
+                Json.obj("definition" -> dt)
+              },
+              "sources" -> Json.arr(
+                Json.obj("name" -> name, "attribution" -> s"<i>$name</i>")
               )
             )
           )
         )
+      }*/
+
+      (XMLDoc \\ "FONT").map { node =>
+        val word = node.text
+        val start = text.indexOfSlice(word)
+        val end = start + word.length
+
+        val gloss = Json.obj(
+          "start" -> start,
+          "end" -> end,
+          "lemmas" -> Json.arr()
+        )
+
+        JAnalysis(start, end, word, gloss)
+      }
+    }.getOrElse(Nil)
+  }
+
+  /**
+   *  Translate From Japanese for all of the languages in the list above.
+   *  TODO: Add back in to-Japanese translations without segmentation
+   */
+  def getWordDefinitions(text: String, dictCode: String): Seq[JsObject] = {
+    getTokens(text).map { token =>
+      val query = WS.url(s"$endpoint?${dictCode}ZUQ$text").get()
+      val lemmas = getXML(query).map { XMLDoc =>
+        var raw = (XMLDoc \\ "pre").text.split("\n")
+        val defins = raw.flatMap(_.split("""[,\s\/]*\(\d+\)"""))
+                        .map(_.trim).distinct
+                        .filterNot(_ == "")
+
+        if (defins.length == 0) None
+        else {
+          val lemma = Json.obj(
+            "representations" -> Json.arr("Orthographic"),
+            "lemmaForm" -> "lemma",
+            "forms" -> Json.obj(
+              "lemma" -> Json.obj(
+                "Orthographic" -> Json.arr(token.lemma)
+              )
+            ),
+            "senses" -> defins.map { text =>
+              Json.obj("definition" -> text)
+            },
+            "sources" -> Json.arr(
+              Json.obj("name" -> name, "attribution" -> s"<i>$name</i>")
+            )
+          )
+          Some(lemma)
+        }
+      }.collect { case Some(lemma) => lemma }
+
+      Json.obj(
+        "start" -> token.start,
+        "end" -> token.end,
+        "lemmas" -> lemmas
       )
     }
+  }
+
+  def translate(user: User, src: String, dst: String, text: String)
+               (implicit request: RequestHeader, restart: TRestart) = {
+    if (src == "ja") {
+      codeMap.get(dst).flatMap { dcode =>
+        val words = getWordDefinitions(text, dcode)
+        if(words.size == 0) None
+        else Some(Json.obj("words" -> words))
+      }
+    } else None
   }
 }
