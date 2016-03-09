24aaa2bdc18568a2044ab0d431390fe88bfe75e2
Logan Kearsley <chronosurfer@gmail.com>
2016-03-09 16:38:47 -0700

    Collins compiles.
    Doesn't actually return anything yet.

--- app/controllers/services/Collins.scala
+++ app/controllers/services/Collins.scala
@@ -1,8 +1,12 @@
-/* package controllers
+package controllers
 
+import Utils._
+import models.User
 import java.net.URLEncoder
 import scala.concurrent.{ExecutionContext, Future, Await}
 import scala.concurrent.duration._
+import scala.xml._
+import play.api.mvc._
 import play.api.libs.json._
 import play.api.libs.ws.WS
 import play.api.Play.{current, configuration}
@@ -11,22 +15,59 @@ import controllers.LangCodes._
 
 object LookupCollins extends Translator {
 
-val name = "Collins";
-val dictionaryCode = "american-learner";
-val searchWord = "";
-val format = "xml";
+  val name = "Collins"
+  val expiration = Utils.getExpiration("Collins")
+  val codeFormat = 'iso639_3
+  
+  val baseURL = "https://api.collinsdictionary.com/api/v1/dictionaries/"
+  val attribution = """<a href="http://www.collinsdictionary.com">
+                         www.Collinsdictionary.com
+                       </a> © HarperCollins Publishers Ltd 2014"""
 
-def requestEntries(key: String, scode: String, dcode: String, text: String){
-val query = WS.url("https://api.collinsdictionary.com/api/v1")
-.withQueryString("dictionaryCode" -> "american-learner", "searchWord" -> text, "format"-> "xml").get()
+  val key = configuration.getString("collins.key")
 
-val result = Await.result(query, Duration.Inf)
-val json = result.json.as[JsObject]
+  def getPairs = Set(("eng", "eng"))
 
-if(result.status != 200) None
-else{}
+  def listEntries(key: String, dict: String, text: String) = {
+    val query = WS.url(s"$baseURL$dict/search")
+                  .withHeaders(
+					"accessKey" -> key,
+					"accept" -> "application/json"
+				  )
+                  .withQueryString("q" -> text)
+				  .get()
 
+    val result = Await.result(query, Duration.Inf)
+	if(result.status != 200) Nil
+	else {
+	  val urls = (result.json \\ "entryUrlresults")
+	               .flatMap(_.asOpt[String].toList)
+	  urls.flatMap { url =>
+	    val query = WS.url(url)
+                  .withHeaders(
+					"accessKey" -> key,
+					"accept" -> "application/json"
+				  )
+                  .withQueryString("format" -> "xml")
+				  .get()
+
+        val result = Await.result(query, Duration.Inf)
+	    if(result.status != 200) Nil
+		else {
+		  (result.json \ "entryContent").asOpt[String].flatMap { xmlstr =>
+		    import scala.util.Try
+			Try(XML.loadString(xmlstr)).toOption
+		  }.toList
+		}
+	  }
+	}
+  }
+
+  def translate(user: User, scode: String, dcode: String, text: String)
+               (implicit request: RequestHeader, restart: TRestart) = {
+    key.flatMap { keystr =>
+      val xmlEntries = listEntries(keystr, "american-learner", text)
+	  None
+	}
+  }
 }
-
-}
-*/
